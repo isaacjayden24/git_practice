095e4dd04e02a276cf99e5fbb74a9c8072fb5e50
Isaac kariithi <isaacjayden27@gmail.com>
2023-11-11 14:02:58 +0300

    second practice

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+data class Person(var firstName: String?, var lastName: String?, var profession: String?, var stack: String?)
+
+fun main(args: Array<String>) {
+    val isaac: Person? = null
+
+    isaac?.apply {
+        firstName = "Isaac"
+        lastName = "Kariithi"
+        profession = "Android developer"
+        stack = "Kotlin and Java"
+    }
+
+    println(isaac)
+
+}
