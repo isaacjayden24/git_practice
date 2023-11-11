313285ad8acc0946619c9362d227fcc9cd69ac94
Isaac kariithi <isaacjayden27@gmail.com>
2023-11-11 14:38:09 +0300

    added a file

---
+++ src/main/kotlin/added.kt
@@ -0,0 +1,14 @@
+data class Course(var courseName: String?, var courseTitle:String)
+
+fun main(args: Array<String>) {
+    val techie: Course? = null
+
+    techie?.apply {
+         courseName = "Computer science"
+        courseTitle = "Android development with kotlin"
+
+    }
+
+    println(techie)
+//introduced this code for some testing dev!!
+}
